26f61d480412b0a8b54689c5f6a71b400d5c8354
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2022-04-24 21:45:14 +0530

    Add files via upload

--- components/Herobanner.jsx
+++ components/Herobanner.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import Link from "next/link";
 const Herobanner = () => {
   return (
-    <div>Herobanner</div>
-  )
-}
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>Mid text</h3>
+        <img src="" alt="headphones" className="hero-banner-image" />
+        <div>
+          <Link href="/product/ID">
+            <button type="button">Button TEXT</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>DESC</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Herobanner
+export default Herobanner;
